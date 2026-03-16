9bd4de55e88ebd76c06afdfa759d7a6eb4e49ccc
safetyIraq <paidhydr@gmail.com>
2026-03-17 01:01:59 +0300

    Update AccessibilityService.java

--- app/src/main/java/com/v8/global/sniffer/AccessibilityService.java
+++ app/src/main/java/com/v8/global/sniffer/AccessibilityService.java
@@ -1,21 +1,14 @@
 package com.v8.global.sniffer;
 
-import android.accessibilityservice.AccessibilityService;
 import android.view.accessibility.AccessibilityEvent;
 
-public class AccessibilityService extends android.accessibilityservice.AccessibilityService {
-    
+public class MyAccessibilityService extends android.accessibilityservice.AccessibilityService {
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        // معالجة أحداث الوصول
+        // هنا يتم صيد حركات الشاشة
     }
-    
+
     @Override
     public void onInterrupt() {
-    }
-    
-    @Override
-    public void onCreate() {
-        super.onCreate();
     }
 }
