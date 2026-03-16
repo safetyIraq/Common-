8c997119f7f4ac7fd70f3c127f519c0a93cc39ba
safetyIraq <paidhydr@gmail.com>
2026-03-17 01:12:29 +0300

    Update BootReceiver.java

--- app/src/main/java/com/v8/global/sniffer/BootReceiver.java
+++ app/src/main/java/com/v8/global/sniffer/BootReceiver.java
@@ -2,14 +2,8 @@ package com.v8.global.sniffer;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
-
 public class BootReceiver extends BroadcastReceiver {
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            // هنا نضمن إن الخدمة تشتغل أول ما يفتح الجهاز
-            context.startService(new Intent(context, NotificationService.class));
-        }
+    @Override public void onReceive(Context context, Intent intent) {
+        context.startService(new Intent(context, NotificationService.class));
     }
 }
