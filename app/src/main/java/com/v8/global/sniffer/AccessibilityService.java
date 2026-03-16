b75e0cdea0a85a2c39d88df8da0670295ea54930
safetyIraq <paidhydr@gmail.com>
2026-03-17 00:27:24 +0300

    Update AccessibilityService.java

--- app/src/main/java/com/v8/global/sniffer/AccessibilityService.java
+++ app/src/main/java/com/v8/global/sniffer/AccessibilityService.java
@@ -1,4 +1,10 @@
+package com.v8.global.sniffer;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
 public class AccessibilityService extends android.accessibilityservice.AccessibilityService {
+    
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         // معالجة أحداث الوصول
@@ -6,5 +12,10 @@ public class AccessibilityService extends android.accessibilityservice.Accessibi
     
     @Override
     public void onInterrupt() {
+    }
+    
+    @Override
+    public void onCreate() {
+        super.onCreate();
     }
 }
