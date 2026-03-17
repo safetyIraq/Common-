3d58c4b23283852484c45875155ce04676e03ddb
safetyIraq <paidhydr@gmail.com>
2026-03-17 08:29:27 +0300

    Update SplashActivity.java

--- app/src/main/java/com/v8/global/sniffer/SplashActivity.java
+++ app/src/main/java/com/v8/global/sniffer/SplashActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import com.v8.global.sniffer.game.MainGameActivity;
 
 public class SplashActivity extends Activity {
 
@@ -11,9 +12,13 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        // فتح النشاط الرئيسي فوراً
-        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainGameActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
     }
 }
