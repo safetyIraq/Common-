9c76b5d988f0e1ea62d091ae224ee311fd10fff5
safetyIraq <paidhydr@gmail.com>
2026-03-17 08:27:58 +0300

    Update GameSettingsActivity.java

--- app/src/main/java/com/v8/global/sniffer/game/GameSettingsActivity.java
+++ app/src/main/java/com/v8/global/sniffer/game/GameSettingsActivity.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 import com.v8.global.sniffer.R;
