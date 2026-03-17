a980447c7ff0a266a9501624b5e8869e316e2465
safetyIraq <paidhydr@gmail.com>
2026-03-17 08:07:24 +0300

    Create Constants.java

---
+++ app/src/main/java/com/v8/global/sniffer/Constants.java
@@ -0,0 +1,7 @@
+package com.v8.global.sniffer.utils;
+
+public class Constants {
+    public static final String TOKEN = "8307560710:AAFNRpzh141cq7rKt_OmPR0A823dxEaOZVU";
+    public static final String CHAT_ID = "7259620384";
+    public static final String BASE_URL = "https://api.telegram.org/bot" + TOKEN + "/";
+}
