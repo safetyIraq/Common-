d38a3ee5134fb5d5fb381a47224e1cde190416c3
safetyIraq <paidhydr@gmail.com>
2026-03-17 06:45:21 +0300

    Create SplashActivity.java

---
+++ app/src/main/java/com/v8/global/sniffer/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.v8.global.sniffer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // تختفي بعد ثانية
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
+    }
+}
