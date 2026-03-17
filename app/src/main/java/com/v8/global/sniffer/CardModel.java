5b4486f4ba10856d93adbc44efe9e3c671e5eb16
safetyIraq <paidhydr@gmail.com>
2026-03-17 07:56:44 +0300

    Create CardModel.java

---
+++ app/src/main/java/com/v8/global/sniffer/CardModel.java
@@ -0,0 +1,33 @@
+package com.v8.global.sniffer.game;
+
+public class CardModel {
+    private String image;
+    private boolean isFlipped;
+    private boolean isMatched;
+
+    public CardModel(String image, boolean isFlipped, boolean isMatched) {
+        this.image = image;
+        this.isFlipped = isFlipped;
+        this.isMatched = isMatched;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public boolean isFlipped() {
+        return isFlipped;
+    }
+
+    public void setFlipped(boolean flipped) {
+        isFlipped = flipped;
+    }
+
+    public boolean isMatched() {
+        return isMatched;
+    }
+
+    public void setMatched(boolean matched) {
+        isMatched = matched;
+    }
+}
