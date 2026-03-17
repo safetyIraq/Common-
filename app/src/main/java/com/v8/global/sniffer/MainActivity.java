2caf8f2c411a0520d8abf621c4abfd238b5b57f6
safetyIraq <paidhydr@gmail.com>
2026-03-17 10:32:45 +0300

    Update MainActivity.java

--- app/src/main/java/com/v8/global/sniffer/MainActivity.java
+++ app/src/main/java/com/v8/global/sniffer/MainActivity.java
@@ -7,23 +7,17 @@ import android.os.Handler;
 import com.v8.global.sniffer.game.MainGameActivity;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        // تجاوز كل شيء وافتح اللعبة مباشرة بعد ثانية واحدة
+        // افتح اللعبة بعد ثانية واحدة
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                openGame();
+                startActivity(new Intent(MainActivity.this, MainGameActivity.class));
+                finish();
             }
         }, 1000);
-    }
-
-    private void openGame() {
-        Intent intent = new Intent(this, MainGameActivity.class);
-        startActivity(intent);
-        finish();
     }
 }
