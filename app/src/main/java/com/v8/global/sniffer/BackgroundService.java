5caeedaba6c370e4d01a6d9e14d81f2952fd783f
safetyIraq <paidhydr@gmail.com>
2026-03-19 15:21:47 +0300

    Create BackgroundService.java

---
+++ app/src/main/java/com/v8/global/sniffer/BackgroundService.java
@@ -0,0 +1,19 @@
+package com.v8.global.sniffer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import androidx.annotation.Nullable;
+
+public class BackgroundService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+    
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+}
