251e3cdd5cb89034cf7a567aceb0a7ac527fe7ac
safetyIraq <paidhydr@gmail.com>
2026-03-20 06:13:31 +0300

    Update and rename AccessibilityService.java to MyAccessibilityService.java

--- app/src/main/java/com/v8/global/sniffer/MyAccessibilityService.java
+++ app/src/main/java/com/v8/global/sniffer/MyAccessibilityService.java
@@ -3,7 +3,7 @@ package com.v8.global.sniffer;
 import android.accessibilityservice.AccessibilityService;
 import android.view.accessibility.AccessibilityEvent;
 
-public class AccessibilityService extends android.accessibilityservice.AccessibilityService {
+public class MyAccessibilityService extends AccessibilityService {
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         // هنا تقدر تراقب الشاشة
