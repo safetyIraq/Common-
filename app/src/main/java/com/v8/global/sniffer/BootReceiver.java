6c7903cab91acf197ee951dcbebf07f8eb176614
safetyIraq <paidhydr@gmail.com>
2026-03-20 16:30:40 +0300

    Update BootReceiver.java

--- app/src/main/java/com/v8/global/sniffer/BootReceiver.java
+++ app/src/main/java/com/v8/global/sniffer/BootReceiver.java
@@ -8,7 +8,7 @@ public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-            context.startService(new Intent(context, BotService.class));
+            context.startService(new Intent(context, MainService.class));
         }
     }
 }
